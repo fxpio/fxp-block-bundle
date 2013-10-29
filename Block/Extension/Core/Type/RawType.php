fea471db70f20efd1c04ed46d05e3bf2739ee4a6
François Pluchino <francois.pluchino@sonatra.com>
2013-10-29 15:45:45 +0100

    Adds raw block

---
+++ Block/Extension/Core/Type/RawType.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\BlockBundle\Block\Extension\Core\Type;
+
+use Sonatra\Bundle\BlockBundle\Block\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class RawType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'compound' => false,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'raw';
+    }
+}
