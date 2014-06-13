12118bb4d1b20f569fc819000ad400530e480a92
François Pluchino <francois.pluchino@sonatra.com>
2014-06-13 15:37:13 +0200

    Adds block builder tests

---
+++ Tests/Block/BlockBuilderTest.php
@@ -0,0 +1,367 @@
+<?php
+
+/**
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\BlockBundle\Tests\Block;
+
+use Sonatra\Bundle\BlockBundle\Block\BlockBuilder;
+use Sonatra\Bundle\BlockBundle\Block\BlockBuilderInterface;
+use Sonatra\Bundle\BlockBundle\Block\BlockFactoryInterface;
+use Sonatra\Bundle\BlockBundle\Block\DataMapperInterface;
+use Sonatra\Bundle\BlockBundle\Block\ResolvedBlockTypeInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class BlockBuilderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var EventDispatcherInterface
+     */
+    protected $dispatcher;
+
+    /**
+     * @var BlockFactoryInterface
+     */
+    protected $factory;
+
+    /**
+     * @var BlockBuilderInterface
+     */
+    protected $builder;
+
+    protected function setUp()
+    {
+        /* @var EventDispatcherInterface $dispatcher */
+        $this->dispatcher = $this->getMock('Symfony\Component\EventDispatcher\EventDispatcherInterface');
+        /* @var BlockFactoryInterface $factory */
+        $this->factory = $this->getMock('Sonatra\Bundle\BlockBundle\Block\BlockFactoryInterface');
+
+        $this->builder = new BlockBuilder('name', null, $this->dispatcher, $this->factory);
+    }
+
+    protected function tearDown()
+    {
+        $this->dispatcher = null;
+        $this->factory = null;
+        $this->builder = null;
+    }
+
+    /**
+     * Changing the name is not allowed, otherwise the name and property path
+     * are not synchronized anymore
+     *
+     * @see BlockType::buildBlock
+     */
+    public function testNoSetName()
+    {
+        $this->assertFalse(method_exists($this->builder, 'setName'));
+    }
+
+    public function testAddNameNoStringAndNoInteger()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\UnexpectedTypeException');
+        $this->builder->add(true);
+    }
+
+    public function testAddTypeNoString()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\UnexpectedTypeException');
+        $this->builder->add('foo', 1234);
+    }
+
+    public function testAddWithGuessFluent()
+    {
+        $this->builder = new BlockBuilder('name', 'stdClass', $this->dispatcher, $this->factory);
+        $builder = $this->builder->add('foo');
+        $this->assertSame($builder, $this->builder);
+    }
+
+    public function testAddIsFluent()
+    {
+        $builder = $this->builder->add('foo', 'text', array('bar' => 'baz'));
+        $this->assertSame($builder, $this->builder);
+    }
+
+    public function testAdd()
+    {
+        $this->assertFalse($this->builder->has('foo'));
+        $this->builder->add('foo', 'text');
+        $this->assertTrue($this->builder->has('foo'));
+    }
+
+    public function testAll()
+    {
+        /* @var \PHPUnit_Framework_MockObject_MockObject $factory */
+        $factory = $this->factory;
+
+        $factory->expects($this->once())
+            ->method('createNamedBuilder')
+            ->with('foo', 'text')
+            ->will($this->returnValue(new BlockBuilder('foo', null, $this->dispatcher, $this->factory)));
+
+        $this->assertCount(0, $this->builder->all());
+        $this->assertFalse($this->builder->has('foo'));
+
+        $this->builder->add('foo', 'text');
+        $children = $this->builder->all();
+
+        $this->assertTrue($this->builder->has('foo'));
+        $this->assertCount(1, $children);
+        $this->assertArrayHasKey('foo', $children);
+    }
+
+    public function testMaintainOrderOfLazyAndExplicitChildren()
+    {
+        $this->builder->add('foo', 'text');
+        $this->builder->add($this->getBlockBuilder('bar'));
+        $this->builder->add('baz', 'text');
+
+        $children = $this->builder->all();
+
+        $this->assertSame(array('foo', 'bar', 'baz'), array_keys($children));
+    }
+
+    public function testAddFormType()
+    {
+        $this->assertFalse($this->builder->has('foo'));
+        $this->builder->add('foo', $this->getMock('Sonatra\Bundle\BlockBundle\Block\BlockTypeInterface'));
+        $this->assertTrue($this->builder->has('foo'));
+    }
+
+    public function testRemove()
+    {
+        $this->builder->add('foo', 'text');
+        $this->builder->remove('foo');
+        $this->assertFalse($this->builder->has('foo'));
+    }
+
+    public function testRemoveUnknown()
+    {
+        $this->builder->remove('foo');
+        $this->assertFalse($this->builder->has('foo'));
+    }
+
+    public function testRemoveAndGetForm()
+    {
+        $this->builder->add('foo', 'text');
+        $this->builder->remove('foo');
+        $block = $this->builder->getBlock();
+        $this->assertInstanceOf('Sonatra\Bundle\BlockBundle\Block\Block', $block);
+    }
+
+    public function testCreateNoTypeNo()
+    {
+        /* @var \PHPUnit_Framework_MockObject_MockObject $factory */
+        $factory = $this->factory;
+
+        $factory->expects($this->once())
+            ->method('createNamedBuilder')
+            ->with('foo', 'text', null, array())
+        ;
+
+        $this->builder->create('foo');
+    }
+
+    public function testGetUnknown()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\InvalidArgumentException', 'The child with the name "foo" does not exist.');
+        $this->builder->get('foo');
+    }
+
+    public function testGetExplicitType()
+    {
+        /* @var \PHPUnit_Framework_MockObject_MockObject $factory */
+        $factory = $this->factory;
+        $expectedType = 'text';
+        $expectedName = 'foo';
+        $expectedOptions = array('bar' => 'baz');
+
+        $factory->expects($this->once())
+            ->method('createNamedBuilder')
+            ->with($expectedName, $expectedType, null, $expectedOptions)
+            ->will($this->returnValue($this->getBlockBuilder()));
+
+        $this->builder->add($expectedName, $expectedType, $expectedOptions);
+        $builder = $this->builder->get($expectedName);
+
+        $this->assertNotSame($builder, $this->builder);
+        $this->assertSame($builder, $this->builder->get($expectedName));
+    }
+
+    public function testGetGuessedType()
+    {
+        /* @var \PHPUnit_Framework_MockObject_MockObject $factory */
+        $factory = $this->factory;
+        $expectedName = 'foo';
+        $expectedOptions = array('bar' => 'baz');
+
+        $factory->expects($this->once())
+            ->method('createBuilderForProperty')
+            ->with('stdClass', $expectedName, null, $expectedOptions)
+            ->will($this->returnValue($this->getBlockBuilder()));
+
+        $this->builder = new BlockBuilder('name', 'stdClass', $this->dispatcher, $this->factory);
+        $this->builder->add($expectedName, null, $expectedOptions);
+        $builder = $this->builder->get($expectedName);
+
+        $this->assertNotSame($builder, $this->builder);
+        $this->assertSame($builder, $this->builder->get($expectedName));
+    }
+
+    public function testGetFormConfigErasesReferences()
+    {
+        $builder = new BlockBuilder('name', null, $this->dispatcher, $this->factory);
+        $builder->add(new BlockBuilder('child', null, $this->dispatcher, $this->factory));
+
+        $config = $builder->getBlockConfig();
+        $reflClass = new \ReflectionClass($config);
+        $children = $reflClass->getProperty('children');
+        $unresolvedChildren = $reflClass->getProperty('unresolvedChildren');
+
+        $children->setAccessible(true);
+        $unresolvedChildren->setAccessible(true);
+
+        $this->assertEmpty($children->getValue($config));
+        $this->assertEmpty($unresolvedChildren->getValue($config));
+    }
+
+    public function testAddChildWithoutName()
+    {
+        $this->builder->add(null, 'text');
+        $this->assertEquals(1, $this->builder->count());
+        $this->assertNotNull(array_keys($this->builder->all())[0]);
+    }
+
+    public function testGetBlockFactory()
+    {
+        $this->assertInstanceOf('Sonatra\Bundle\BlockBundle\Block\BlockFactoryInterface', $this->builder->getBlockFactory());
+    }
+
+    public function testGetIterator()
+    {
+        /* @var BlockBuilder $builder */
+        $builder = $this->builder;
+        $this->assertInstanceOf('ArrayIterator', $builder->getIterator());
+    }
+
+    public function testGetCount()
+    {
+        $this->assertEquals(0, $this->builder->count());
+    }
+
+    public function testGetBlock()
+    {
+        /* @var DataMapperInterface $dataMapper */
+        $dataMapper = $this->getMock('Sonatra\Bundle\BlockBundle\Block\DataMapperInterface');
+        /* @var ResolvedBlockTypeInterface $blockType */
+        $blockType = $this->getMock('Sonatra\Bundle\BlockBundle\Block\ResolvedBlockTypeInterface');
+
+        $builder = new BlockBuilder('name', null, $this->dispatcher, $this->factory);
+        $child = new BlockBuilder('child', null, $this->dispatcher, $this->factory);
+
+        $builder->setCompound(true);
+        $builder->setDataMapper($dataMapper);
+        $builder->setType($blockType);
+        $child->setType($blockType);
+
+        $builder->add($child);
+        $block = $builder->getBlock();
+
+        $this->assertInstanceOf('Sonatra\Bundle\BlockBundle\Block\Block', $block);
+        $this->assertEquals(1, $block->count());
+    }
+
+    public function testAddTypeAfterGetBlock()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+        $config = $this->builder->getBlockConfig();
+        /* @var BlockBuilder $config */
+        $config->add('foo', 'text');
+    }
+
+    public function testCreateTypeAfterGetBlock()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+        $config = $this->builder->getBlockConfig();
+        /* @var BlockBuilder $config */
+        $config->create('foo', 'text');
+    }
+
+    public function testGetTypeAfterGetBlock()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+        $config = $this->builder->getBlockConfig();
+        /* @var BlockBuilder $config */
+        $config->get('foo');
+    }
+
+    public function testRemoveTypeAfterGetBlock()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+        $config = $this->builder->getBlockConfig();
+        /* @var BlockBuilder $config */
+        $config->remove('foo');
+    }
+
+    public function testHasTypeAfterGetBlock()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+        $config = $this->builder->getBlockConfig();
+        /* @var BlockBuilder $config */
+        $config->has('foo');
+    }
+
+    public function testAllTypeAfterGetBlock()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+        $config = $this->builder->getBlockConfig();
+        /* @var BlockBuilder $config */
+        $config->all();
+    }
+
+    public function testCountTypeAfterGetBlock()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+        $config = $this->builder->getBlockConfig();
+        /* @var BlockBuilder $config */
+        $config->count();
+    }
+
+    public function testGetBlockTypeAfterGetBlock()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+        $config = $this->builder->getBlockConfig();
+        /* @var BlockBuilder $config */
+        $config->getBlock();
+    }
+
+    public function testGetIteratorTypeAfterGetBlock()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+        /* @var BlockBuilder $config */
+        $config = $this->builder->getBlockConfig();
+        $config->getIterator();
+    }
+
+    private function getBlockBuilder($name = 'name')
+    {
+        $mock = $this->getMockBuilder('Sonatra\Bundle\BlockBundle\Block\BlockBuilder')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mock->expects($this->any())
+            ->method('getName')
+            ->will($this->returnValue($name));
+
+        return $mock;
+    }
+}
---
+++ Tests/Block/BlockConfigBuilderTest.php
@@ -0,0 +1,348 @@
+<?php
+
+/**
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\BlockBundle\Tests\Block;
+
+use Sonatra\Bundle\BlockBundle\Block\BlockConfigBuilder;
+use Sonatra\Bundle\BlockBundle\Block\BlockConfigBuilderInterface;
+use Sonatra\Bundle\BlockBundle\Block\DataMapperInterface;
+use Sonatra\Bundle\BlockBundle\Block\DataTransformerInterface;
+use Sonatra\Bundle\BlockBundle\Block\ResolvedBlockTypeInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormInterface;
+
+/**
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class BlockConfigBuilderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var EventDispatcherInterface
+     */
+    protected $dispatcher;
+
+    /**
+     * @var BlockConfigBuilderInterface
+     */
+    protected $config;
+
+    protected function setUp()
+    {
+        $this->dispatcher = $this->getMock('Symfony\Component\EventDispatcher\EventDispatcherInterface');
+        /* @var EventDispatcherInterface $dispatcher */
+        $dispatcher = $this->dispatcher;
+        $options = array(
+            'foo' => 'bar',
+        );
+
+        $this->config = new BlockConfigBuilder('name', null, $dispatcher, $options);
+    }
+
+    protected function tearDown()
+    {
+        $this->dispatcher = null;
+        $this->config = null;
+    }
+
+    public function testNotStringName()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\UnexpectedTypeException');
+
+        new BlockConfigBuilder(42, null, $this->dispatcher);
+    }
+
+    public function testInvalidName()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\InvalidArgumentException');
+
+        new BlockConfigBuilder('foo@bar', null, $this->dispatcher);
+    }
+
+    public function testInvalidClassname()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\InvalidArgumentException');
+
+        new BlockConfigBuilder('name', 'Foobar', $this->dispatcher);
+    }
+
+    public function testAddEvents()
+    {
+        /* @var EventSubscriberInterface $subscriber */
+        $subscriber = $this->getMock('Symfony\Component\EventDispatcher\EventSubscriberInterface');
+
+        $this->config->addEventListener('foo', function () {}, 0);
+        $this->config->addEventSubscriber($subscriber);
+    }
+
+    public function testViewTransformers()
+    {
+        /* @var DataTransformerInterface $dataTransformer */
+        $dataTransformer = $this->getMock('Sonatra\Bundle\BlockBundle\Block\DataTransformerInterface');
+        /* @var DataTransformerInterface $dataTransformer2 */
+        $dataTransformer2 = $this->getMock('Sonatra\Bundle\BlockBundle\Block\DataTransformerInterface');
+
+        $this->config->addViewTransformer($dataTransformer);
+        $this->config->addViewTransformer($dataTransformer2, true);
+        $this->assertCount(2, $this->config->getViewTransformers());
+        $this->assertSame($dataTransformer, $this->config->getViewTransformers()[1]);
+        $this->assertSame($dataTransformer2, $this->config->getViewTransformers()[0]);
+
+        $this->config->resetViewTransformers();
+        $this->assertCount(0, $this->config->getViewTransformers());
+    }
+
+    public function testModelTransformers()
+    {
+        /* @var DataTransformerInterface $dataTransformer */
+        $dataTransformer = $this->getMock('Sonatra\Bundle\BlockBundle\Block\DataTransformerInterface');
+        /* @var DataTransformerInterface $dataTransformer2 */
+        $dataTransformer2 = $this->getMock('Sonatra\Bundle\BlockBundle\Block\DataTransformerInterface');
+
+        $this->config->addModelTransformer($dataTransformer);
+        $this->config->addModelTransformer($dataTransformer2, true);
+        $this->assertCount(2, $this->config->getModelTransformers());
+        $this->assertSame($dataTransformer, $this->config->getModelTransformers()[0]);
+        $this->assertSame($dataTransformer2, $this->config->getModelTransformers()[1]);
+
+        $this->config->resetModelTransformers();
+        $this->assertCount(0, $this->config->getModelTransformers());
+    }
+
+    public function testGettersAndSetters()
+    {
+        //TODO
+        /* @var ResolvedBlockTypeInterface $type */
+        $type = $this->getMock('Sonatra\Bundle\BlockBundle\Block\ResolvedBlockTypeInterface');
+        /* @var DataMapperInterface $dataMapper */
+        $dataMapper = $this->getMock('Sonatra\Bundle\BlockBundle\Block\DataMapperInterface');
+        /* @var FormInterface $form */
+        $form = $this->getMock('Symfony\Component\Form\FormInterface');
+
+        $this->assertEquals('name', $this->config->getName());
+        $this->assertSame($this->dispatcher, $this->config->getEventDispatcher());
+        $this->assertEquals(array('foo' => 'bar'), $this->config->getOptions());
+        $this->assertTrue($this->config->hasOption('foo'));
+        $this->assertFalse($this->config->hasOption('baz'));
+        $this->assertEquals('bar', $this->config->getOption('foo'));
+        $this->assertEquals('bar', $this->config->getOption('baz', 'bar'));
+
+        $this->assertSame($this->config, $this->config->setPropertyPath('field'));
+        $this->assertInstanceOf('Symfony\Component\PropertyAccess\PropertyPath', $this->config->getPropertyPath());
+        $this->assertSame($this->config, $this->config->setMapped(true));
+        $this->assertTrue($this->config->getMapped());
+        $this->assertSame($this->config, $this->config->setInheritData(true));
+        $this->assertTrue($this->config->getInheritData());
+        $this->assertSame($this->config, $this->config->setCompound(true));
+        $this->assertTrue($this->config->getCompound());
+        $this->assertSame($this->config, $this->config->setType($type));
+        $this->assertSame($type, $this->config->getType());
+        $this->assertSame($this->config, $this->config->setDataMapper($dataMapper));
+        $this->assertSame($dataMapper, $this->config->getDataMapper());
+        $this->assertSame($this->config, $this->config->setEmptyData('empty'));
+        $this->assertEquals('empty', $this->config->getEmptyData());
+        $this->assertFalse($this->config->hasAttribute('foo'));
+        $this->assertSame($this->config, $this->config->setAttribute('foo', 'bar'));
+        $this->assertTrue($this->config->hasAttribute('foo'));
+        $this->assertEquals(array('foo' => 'bar'), $this->config->getAttributes());
+        $this->assertSame($this->config, $this->config->setAttributes(array('bar' => 'foo')));
+        $this->assertFalse($this->config->hasAttribute('foo'));
+        $this->assertTrue($this->config->hasAttribute('bar'));
+        $this->assertEquals(array('bar' => 'foo'), $this->config->getAttributes());
+        $this->assertEquals('foo', $this->config->getAttribute('bar'));
+        $this->assertEquals('bar', $this->config->getAttribute('foo', 'bar'));
+
+        $this->assertSame($this->config, $this->config->setDataClass('Foobar'));
+        $this->assertEquals('Foobar', $this->config->getDataClass());
+        $this->assertSame($this->config, $this->config->setData('data'));
+        $this->assertEquals('data', $this->config->getData());
+        $this->assertSame($this->config, $this->config->setForm($form));
+        $this->assertSame($form, $this->config->getForm());
+        $this->assertSame($this->config, $this->config->setData('data'));
+    }
+
+    public function testAddEventListenerAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        $config = $this->getBlockConfig();
+        $config->addEventListener('foo', function () {}, 0);
+    }
+
+    public function testAddEventSubscriberAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        /* @var EventSubscriberInterface $subscriber */
+        $subscriber = $this->getMock('Symfony\Component\EventDispatcher\EventSubscriberInterface');
+
+        $config = $this->getBlockConfig();
+        $config->addEventSubscriber($subscriber);
+    }
+
+    public function testAddViewTransformersAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        /* @var DataTransformerInterface $dataTransformer */
+        $dataTransformer = $this->getMock('Sonatra\Bundle\BlockBundle\Block\DataTransformerInterface');
+
+        $config = $this->getBlockConfig();
+        $config->addViewTransformer($dataTransformer);
+    }
+
+    public function testResetViewTransformersAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        $config = $this->getBlockConfig();
+        $config->resetViewTransformers();
+    }
+
+    public function testAddModelTransformersAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        /* @var DataTransformerInterface $dataTransformer */
+        $dataTransformer = $this->getMock('Sonatra\Bundle\BlockBundle\Block\DataTransformerInterface');
+        $config = $this->getBlockConfig();
+        $config->addModelTransformer($dataTransformer);
+    }
+
+    public function testResetModelTransformersAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        $config = $this->getBlockConfig();
+        $config->resetModelTransformers();
+    }
+
+    public function testGetBlockConfigAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        $config = $this->getBlockConfig();
+        $config->getBlockConfig();
+    }
+
+    public function testSetPropertyPathAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        $config = $this->getBlockConfig();
+        $config->setPropertyPath('field');
+    }
+
+    public function testSetMappedAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        $config = $this->getBlockConfig();
+        $config->setMapped(true);
+    }
+
+    public function testSetInheritDataAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        $config = $this->getBlockConfig();
+        $config->setInheritData(true);
+    }
+
+    public function testSetCompoundAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        $config = $this->getBlockConfig();
+        $config->setCompound(true);
+    }
+
+    public function testSetTypeAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        /* @var ResolvedBlockTypeInterface $type */
+        $type = $this->getMock('Sonatra\Bundle\BlockBundle\Block\ResolvedBlockTypeInterface');
+
+        $config = $this->getBlockConfig();
+        $config->setType($type);
+    }
+
+    public function testSetDataMapperAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        /* @var DataMapperInterface $dataMapper */
+        $dataMapper = $this->getMock('Sonatra\Bundle\BlockBundle\Block\DataMapperInterface');
+
+        $config = $this->getBlockConfig();
+        $config->setDataMapper($dataMapper);
+    }
+
+    public function testSetEmptyDataAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        $config = $this->getBlockConfig();
+        $config->setEmptyData('empty');
+    }
+
+    public function testSetAttributeAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        $config = $this->getBlockConfig();
+        $config->setAttribute('foo', 'bar');
+    }
+
+    public function testSetAttributesAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        $config = $this->getBlockConfig();
+        $config->setAttributes(array('foo' => 'bar'));
+    }
+
+    public function testSetDataAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        $config = $this->getBlockConfig();
+        $config->setData('data');
+    }
+
+    public function testSetDataClassAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        $config = $this->getBlockConfig();
+        $config->setDataClass('Foobar');
+    }
+
+    public function testSetFormAfterGetBlockConfig()
+    {
+        $this->setExpectedException('Sonatra\Bundle\BlockBundle\Block\Exception\BadMethodCallException');
+
+        /* @var DataMapperInterface $dataMapper */
+        $form = $this->getMock('Symfony\Component\Form\FormInterface');
+
+        $config = $this->getBlockConfig();
+        $config->setForm($form);
+    }
+
+    /**
+     * @return BlockConfigBuilderInterface
+     */
+    private function getBlockConfig()
+    {
+        return $this->config->getBlockConfig();
+    }
+}
