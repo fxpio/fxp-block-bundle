bc223e0d34ce1ff86df8a8453fef96103adc6761
François Pluchino <francois.pluchino@sonatra.com>
2014-06-13 10:11:59 +0200

    Adds block util tests

--- Tests/Block/Util/BlockUtilTest.php
+++ Tests/Block/Util/BlockUtilTest.php
@@ -11,6 +11,7 @@
 
 namespace Sonatra\Bundle\BlockBundle\Tests\Block\Util;
 
+use Sonatra\Bundle\BlockBundle\Block\BlockInterface;
 use Sonatra\Bundle\BlockBundle\Block\Util\BlockUtil;
 
 /**
@@ -20,4 +21,76 @@ use Sonatra\Bundle\BlockBundle\Block\Util\BlockUtil;
  */
 class BlockUtilTest extends \PHPUnit_Framework_TestCase
 {
+    public function testIsEmpty()
+    {
+        $this->assertTrue(BlockUtil::isEmpty(null));
+        $this->assertTrue(BlockUtil::isEmpty(''));
+        $this->assertFalse(BlockUtil::isEmpty('foobar'));
+    }
+
+    public function testCreateUniqueName()
+    {
+        $name = BlockUtil::createUniqueName();
+        $this->assertEquals(0, strpos($name, 'block'));
+
+        $id = substr($name, 5);
+        $this->assertTrue(strlen($id) >= 5);
+    }
+
+    public function testCreateBlockId()
+    {
+        $parentBlock = $this->getMock('Sonatra\Bundle\BlockBundle\Block\BlockInterface');
+        $parentBlock->expects($this->any())
+            ->method('getName')
+            ->will($this->returnValue('foo'));
+
+        $block = $this->getMock('Sonatra\Bundle\BlockBundle\Block\BlockInterface');
+        $block->expects($this->any())
+            ->method('getName')
+            ->will($this->returnValue('bar'));
+        $block->expects($this->any())
+            ->method('getParent')
+            ->will($this->returnValue($parentBlock));
+        $block->expects($this->any())
+            ->method('getOption')
+            ->with($this->equalTo('chained_block'))
+            ->will($this->returnValue(true));
+
+        /* @var BlockInterface $block */
+        $this->assertEquals('foo_bar', BlockUtil::createBlockId($block));
+    }
+
+    public function testIsValidBlock()
+    {
+        $parentType = $this->getMock('Sonatra\Bundle\BlockBundle\Block\ResolvedBlockTypeInterface');
+        $parentType->expects($this->any())
+            ->method('getName')
+            ->will($this->returnValue('foo'));
+
+        $blockType = $this->getMock('Sonatra\Bundle\BlockBundle\Block\ResolvedBlockTypeInterface');
+        $blockType->expects($this->any())
+            ->method('getName')
+            ->will($this->returnValue('bar'));
+        $blockType->expects($this->any())
+            ->method('getParent')
+            ->will($this->returnValue($parentType));
+
+        $blockConfig = $this->getMock('Sonatra\Bundle\BlockBundle\Block\BlockBuilderInterface');
+        $blockConfig->expects($this->any())
+            ->method('getType')
+            ->will($this->returnValue($blockType));
+
+        $block = $this->getMock('Sonatra\Bundle\BlockBundle\Block\BlockInterface');
+        $block->expects($this->any())
+            ->method('getConfig')
+            ->will($this->returnValue($blockConfig));
+
+        /* @var BlockInterface $block */
+        $this->assertTrue(BlockUtil::isValidBlock('foo', $block));
+        $this->assertTrue(BlockUtil::isValidBlock('bar', $block));
+        $this->assertTrue(BlockUtil::isValidBlock(array('foo', 'bar'), $block));
+        $this->assertTrue(BlockUtil::isValidBlock(array('foo', 'baz'), $block));
+        $this->assertFalse(BlockUtil::isValidBlock('baz', $block));
+        $this->assertFalse(BlockUtil::isValidBlock(array('baz', 'boo!'), $block));
+    }
 }
