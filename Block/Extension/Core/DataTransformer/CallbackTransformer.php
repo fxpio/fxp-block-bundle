3176916ac78080e8c9efc70b37ef84142659eb23
François Pluchino <francois.pluchino@sonatra.com>
2014-06-17 14:31:36 +0200

    Changes checkbox value in view

---
+++ Block/Extension/Core/DataTransformer/CallbackTransformer.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\BlockBundle\Block\Extension\Core\DataTransformer;
+
+use Sonatra\Bundle\BlockBundle\Block\DataTransformerInterface;
+use Sonatra\Bundle\BlockBundle\Block\Exception\TransformationFailedException;
+
+/**
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class CallbackTransformer implements DataTransformerInterface
+{
+    /**
+     * @var \Closure
+     */
+    private $transform;
+
+    /**
+     * Constructor.
+     *
+     * @param \Closure $transform The forward transform callback
+     */
+    public function __construct(\Closure $transform)
+    {
+        $this->transform = $transform;
+    }
+
+    /**
+     * Transforms a value from the original representation to a transformed representation.
+     *
+     * @param mixed $data The value in the original representation
+     *
+     * @return mixed The value in the transformed representation
+     *
+     * @throws TransformationFailedException When the transformation fails
+     */
+    public function transform($data)
+    {
+        return call_user_func($this->transform, $data);
+    }
+}
--- Block/Extension/Core/Type/CheckboxType.php
+++ Block/Extension/Core/Type/CheckboxType.php
@@ -12,12 +12,49 @@
 namespace Sonatra\Bundle\BlockBundle\Block\Extension\Core\Type;
 
 use Sonatra\Bundle\BlockBundle\Block\AbstractType;
+use Sonatra\Bundle\BlockBundle\Block\BlockBuilderInterface;
+use Sonatra\Bundle\BlockBundle\Block\BlockInterface;
+use Sonatra\Bundle\BlockBundle\Block\BlockView;
+use Sonatra\Bundle\BlockBundle\Block\Extension\Core\DataTransformer\CallbackTransformer;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
  * @author François Pluchino <francois.pluchino@sonatra.com>
  */
 class CheckboxType extends AbstractType
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function buildBlock(BlockBuilderInterface $builder, array $options)
+    {
+        $builder->addViewTransformer(new CallbackTransformer(
+            function ($value) {
+                return $value ? 'checked' : '';
+            }
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(BlockView $view, BlockInterface $block, array $options)
+    {
+        $view->vars = array_replace($view->vars, array(
+            'checked' => $view->vars['data'],
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data' => false,
+        ));
+    }
+
     /**
      * {@inheritdoc}
      */
