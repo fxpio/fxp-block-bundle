7a45a250a83dcc3a9787dd793c6397f1c0f252e1
François Pluchino <francois.pluchino@sonatra.com>
2014-06-17 14:32:10 +0200

    Adds checkbox type tests

---
+++ Tests/Block/Extension/Core/Type/CheckboxTypeTest.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\BlockBundle\Tests\Block\Extension\Core\Type;
+
+use Sonatra\Bundle\BlockBundle\Block\Extension\Core\DataTransformer\CallbackTransformer;
+use Sonatra\Bundle\BlockBundle\Tests\Block\TypeTestCase;
+
+/**
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class CheckboxTypeTest extends TypeTestCase
+{
+    public function testDataIsFalseByDefault()
+    {
+        $block = $this->factory->create('checkbox');
+
+        $this->assertFalse($block->getData());
+        $this->assertFalse($block->getNormData());
+        $this->assertEquals('', $block->getViewData());
+    }
+
+    public function testCheckedIfDataTrue()
+    {
+        $block = $this->factory->create('checkbox');
+        $block->setData(true);
+        $view = $block->createView();
+
+        $this->assertTrue($view->vars['checked']);
+        $this->assertEquals('checked', $view->vars['value']);
+    }
+
+    public function testNotCheckedIfDataFalse()
+    {
+        $block = $this->factory->create('checkbox');
+        $block->setData(false);
+        $view = $block->createView();
+
+        $this->assertFalse($view->vars['checked']);
+        $this->assertEquals('', $view->vars['value']);
+    }
+
+    public function provideCustomModelTransformerData()
+    {
+        return array(
+            array('checked', true),
+            array('unchecked', false),
+        );
+    }
+
+    /**
+     * @dataProvider provideCustomModelTransformerData
+     */
+    public function testCustomModelTransformer($data, $checked)
+    {
+        // present a binary status field as a checkbox
+        $transformer = new CallbackTransformer(
+            function ($value) {
+                return 'checked' == $value;
+            }
+        );
+
+        $builder = $this->factory->createBuilder('checkbox');
+        $builder->addModelTransformer($transformer);
+        $block = $builder->getBlock();
+
+        $block->setData($data);
+        $view = $block->createView();
+
+        $this->assertSame($data, $block->getData());
+        $this->assertSame($checked, $block->getNormData());
+        $this->assertEquals($checked, $view->vars['checked']);
+    }
+}
