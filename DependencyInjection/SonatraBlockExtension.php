1f38bb8c88ea93f67d7a31ef62c9e6ccec36971b
François Pluchino <francois.pluchino@sonatra.com>
2015-08-19 21:11:05 +0200

    Fix unused use

--- DependencyInjection/SonatraBlockExtension.php
+++ DependencyInjection/SonatraBlockExtension.php
@@ -14,7 +14,6 @@ namespace Sonatra\Bundle\BlockBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 /**
