0e158d4d594aa7f69765869c03a99fa5b8e29b5d
François Pluchino <francois.pluchino@sonatra.com>
2017-06-19 17:07:42 +0200

    Refactorise the block compiler pass

--- DependencyInjection/Compiler/BlockPass.php
+++ DependencyInjection/Compiler/BlockPass.php
@@ -11,8 +11,10 @@
 
 namespace Sonatra\Bundle\BlockBundle\DependencyInjection\Compiler;
 
+use Sonatra\Component\Block\Exception\InvalidArgumentException;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
 /**
  * Adds all services with the tags "sonatra_block.type" and "sonatra_block.type_guesser" as
@@ -37,11 +39,7 @@ class BlockPass implements CompilerPassInterface
         $types = array();
 
         foreach ($container->findTaggedServiceIds('sonatra_block.type') as $serviceId => $tag) {
-            $serviceDefinition = $container->getDefinition($serviceId);
-
-            if (!$serviceDefinition->isPublic()) {
-                throw new \InvalidArgumentException(sprintf('The service "%s" must be public as block types are lazy-loaded.', $serviceId));
-            }
+            $serviceDefinition = $this->getPublicRequireDefinition($container, $serviceId, 'types');
 
             // Support type access by FQCN
             $types[$serviceDefinition->getClass()] = $serviceId;
@@ -52,15 +50,12 @@ class BlockPass implements CompilerPassInterface
         $typeExtensions = array();
 
         foreach ($container->findTaggedServiceIds('sonatra_block.type_extension') as $serviceId => $tag) {
-            $serviceDefinition = $container->getDefinition($serviceId);
-            if (!$serviceDefinition->isPublic()) {
-                throw new \InvalidArgumentException(sprintf('The service "%s" must be public as block type extensions are lazy-loaded.', $serviceId));
-            }
+            $this->getPublicRequireDefinition($container, $serviceId, 'type extensions');
 
             if (isset($tag[0]['extended_type'])) {
                 $extendedType = $tag[0]['extended_type'];
             } else {
-                throw new \InvalidArgumentException(sprintf('Tagged block type extension must have the extended type configured using the extended_type/extended-type attribute, none was configured for the "%s" service.', $serviceId));
+                throw new InvalidArgumentException(sprintf('Tagged block type extension must have the extended type configured using the extended_type/extended-type attribute, none was configured for the "%s" service.', $serviceId));
             }
 
             $typeExtensions[$extendedType][] = $serviceId;
@@ -71,12 +66,31 @@ class BlockPass implements CompilerPassInterface
         // Find all services annotated with "sonatra_block.type_guesser"
         $guessers = array_keys($container->findTaggedServiceIds('sonatra_block.type_guesser'));
         foreach ($guessers as $serviceId) {
-            $serviceDefinition = $container->getDefinition($serviceId);
-            if (!$serviceDefinition->isPublic()) {
-                throw new \InvalidArgumentException(sprintf('The service "%s" must be public as block type guessers are lazy-loaded.', $serviceId));
-            }
+            $this->getPublicRequireDefinition($container, $serviceId, 'type guessers');
         }
 
         $definition->replaceArgument(2, $guessers);
+    }
+
+    /**
+     * Get the service definition and validate if the service is public.
+     *
+     * @param ContainerBuilder $container The container builder
+     * @param string           $id        The service id
+     * @param string           $type      The block type
+     *
+     * @return Definition
+     *
+     * @throws InvalidArgumentException When the service is not public
+     */
+    private function getPublicRequireDefinition(ContainerBuilder $container, $id, $type)
+    {
+        $serviceDefinition = $container->getDefinition($id);
+
+        if (!$serviceDefinition->isPublic()) {
+            throw new InvalidArgumentException(sprintf('The service "%s" must be public as block %s are lazy-loaded.', $id, $type));
+        }
+
+        return $serviceDefinition;
     }
 }
---
+++ Tests/DependencyInjection/Compiler/BlockPassTest.php
@@ -0,0 +1,162 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\BlockBundle\Tests\DependencyInjection\Compiler;
+
+use PHPUnit\Framework\TestCase;
+use Sonatra\Bundle\BlockBundle\DependencyInjection\Compiler\BlockPass;
+use Sonatra\Component\Block\Extension\Core\Type\BlockType;
+use Sonatra\Component\Block\Extension\DependencyInjection\DependencyInjectionExtension;
+use Sonatra\Component\Block\Guess\TypeGuess;
+use Sonatra\Component\Block\Tests\Fixtures\Extension\FooExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * Tests case for Block compiler pass.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class BlockPassTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    protected $rootDir;
+
+    /**
+     * @var Filesystem
+     */
+    protected $fs;
+
+    /**
+     * @var BlockPass
+     */
+    protected $pass;
+
+    protected function setUp()
+    {
+        $this->rootDir = sys_get_temp_dir().'/sonatra_block-bundle_block_pass_tests';
+        $this->fs = new Filesystem();
+        $this->pass = new BlockPass();
+    }
+
+    protected function tearDown()
+    {
+        $this->fs->remove($this->rootDir);
+        $this->pass = null;
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage The service "test.block" must be public as block types are lazy-loaded.
+     */
+    public function testProcessBlockTypeWithNotPublicService()
+    {
+        $container = $this->getContainer();
+
+        $this->assertTrue($container->hasDefinition('sonatra_block.extension'));
+
+        $blockDef = new Definition(BlockType::class);
+        $blockDef->setTags(array('sonatra_block.type' => array()));
+        $blockDef->setPublic(false);
+        $container->setDefinition('test.block', $blockDef);
+
+        $this->pass->process($container);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage The service "test.block_extension" must be public as block type extensions are lazy-loaded.
+     */
+    public function testProcessBlockTypeExtensionWithNotPublicService()
+    {
+        $container = $this->getContainer();
+
+        $this->assertTrue($container->hasDefinition('sonatra_block.extension'));
+
+        $blockDef = new Definition(FooExtension::class);
+        $blockDef->setTags(array('sonatra_block.type_extension' => array()));
+        $blockDef->setPublic(false);
+        $container->setDefinition('test.block_extension', $blockDef);
+
+        $this->pass->process($container);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Tagged block type extension must have the extended type configured using the extended_type/extended-type attribute, none was configured for the "test.block_extension" service.
+     */
+    public function testProcessBlockTypeExtensionWithoutExtendedType()
+    {
+        $container = $this->getContainer();
+
+        $this->assertTrue($container->hasDefinition('sonatra_block.extension'));
+
+        $blockDef = new Definition(FooExtension::class);
+        $blockDef->setTags(array('sonatra_block.type_extension' => array()));
+        $container->setDefinition('test.block_extension', $blockDef);
+
+        $this->pass->process($container);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage The service "test.block_guesser" must be public as block type guessers are lazy-loaded.
+     */
+    public function testProcessBlockTypeGuesserWithNotPublicService()
+    {
+        $container = $this->getContainer();
+
+        $this->assertTrue($container->hasDefinition('sonatra_block.extension'));
+
+        $blockDef = new Definition(TypeGuess::class);
+        $blockDef->setTags(array('sonatra_block.type_guesser' => array()));
+        $blockDef->setPublic(false);
+        $container->setDefinition('test.block_guesser', $blockDef);
+
+        $this->pass->process($container);
+    }
+
+    /**
+     * Gets the container.
+     *
+     * @return ContainerBuilder
+     */
+    protected function getContainer()
+    {
+        $container = new ContainerBuilder(new ParameterBag(array(
+            'kernel.cache_dir' => $this->rootDir.'/cache',
+            'kernel.debug' => false,
+            'kernel.environment' => 'test',
+            'kernel.name' => 'kernel',
+            'kernel.root_dir' => $this->rootDir,
+            'kernel.charset' => 'UTF-8',
+            'assetic.debug' => false,
+            'assetic.cache_dir' => $this->rootDir.'/cache/assetic',
+            'kernel.bundles' => array(),
+        )));
+
+        $extDef = new Definition(DependencyInjectionExtension::class);
+        $extDef->setProperty('container', new Reference('service_container'));
+        $extDef->setArguments(array(
+            array(),
+            array(),
+            array(),
+        ));
+        $container->setDefinition('sonatra_block.extension', $extDef);
+
+        return $container;
+    }
+}
